ec3e2767436e4e81cbbd64d66ef85d3b82688d3c
James Reeves <jreeves@weavejester.com>
2014-06-09 02:36:08 +0100

    Add basic tests

--- test/intentions/core_test.clj
+++ test/intentions/core_test.clj
@@ -2,6 +2,29 @@
   (:require [clojure.test :refer :all]
             [intentions.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defintent valid?
+  :dispatch identity
+  :combine  #(and %1 %2))
+
+(defconduct valid? ::good [_] true)
+
+(defconduct valid? ::bad [_] false)
+
+(derive ::a ::good)
+(derive ::b ::bad)
+(derive ::c ::good)
+(derive ::c ::bad)
+
+(deftest test-intent
+  (is (valid? ::a))
+  (is (not (valid? ::b)))
+  (is (not (valid? ::c))))
+
+(deftest test-conducts
+  (is (map? (conducts valid?)))
+  (is (= (set (keys (conducts valid?))) #{::good ::bad})))
+
+(deftest test-derive-all
+  (derive-all ::foobar #{::foo ::bar})
+  (is (isa? ::foobar ::foo))
+  (is (isa? ::foobar ::bar)))
