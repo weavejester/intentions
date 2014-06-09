d5cd2bb986e52147a1e406f25708130b24f4be4a
James Reeves <jreeves@weavejester.com>
2014-06-09 02:00:03 +0100

    Add minimal defintent and defconduct

--- src/intentions/core.clj
+++ src/intentions/core.clj
@@ -1,6 +1,31 @@
 (ns intentions.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn- conduct-keys [hierarchy dispatch-val]
+  (into #{dispatch-val}
+        (if hierarchy
+          (ancestors hierarchy dispatch-val)
+          (ancestors dispatch-val))))
+
+(defn make-intent
+  [{:keys [dispatch combine default hierarchy]
+    :or   {default :default}}]
+  (let [conducts (atom {})
+        func     (fn [& args]
+                   (->> (apply dispatch args)
+                        (conduct-keys hierarchy)
+                        (keep @conducts)
+                        (map #(apply % args))
+                        (reduce combine)))]
+    (with-meta func
+      (assoc (meta func) ::conducts conducts))))
+
+(defmacro defintent
+  [name & {:as options}]
+  `(def ~name (make-intent ~options)))
+
+(defn add-conduct! [intent dispatch-val dispatch-fn]
+  (swap! (::conducts (meta intent)) assoc dispatch-val dispatch-fn))
+
+(defmacro defconduct
+  [name dispatch-val & fn-tail]
+  `(add-conduct! ~name ~dispatch-val (fn ~@fn-tail)))
