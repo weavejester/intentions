d4a1505094fc21b6424d67fdeaf6242239eab206
James Reeves <jreeves@weavejester.com>
2014-06-11 20:19:37 +0100

    Add description and URL to project

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject intentions "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Multimethods that combine rather than override inherited behavior"
+  :url "https://github.com/weavejester/intentions"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
