64361ba7a8ec4128fef65716509a26b43005bbec
James Reeves <jreeves@weavejester.com>
2014-06-13 16:29:55 +0100

    Refactor make-intent function

--- src/intentions/core.clj
+++ src/intentions/core.clj
@@ -5,7 +5,40 @@
 (defn intent?
   "Returns true if the object is an intention."
   [x]
-  (and (fn? x) (::conducts (meta x))))
+  (and (fn? x) (::state (meta x))))
+
+(defn- dispatch-comparator [isa? prefers]
+  (fn [a b]
+    (cond
+     (or (isa? a b) (prefers [a b])) 1
+     (or (isa? b a) (prefers [b a])) -1
+     :else (compare (str a) (str b)))))
+
+(defn- find-conducts [state dispatch-val isa?]
+  (->> (:conducts state)
+       (filter #(isa? dispatch-val (key %)))
+       (sort-by key (dispatch-comparator isa? (:prefers state)))
+       (mapv val)))
+
+(defn- combine-conducts [combine fs]
+  (fn [& args]
+    (reduce combine (map #(apply % args) fs))))
+
+(defn- conduct-fn [combine default state dispatch-val isa?]
+  (if-let [fs (seq (find-conducts state dispatch-val isa?))]
+    (combine-conducts combine fs)
+    (get-in state [:conducts default])))
+
+(defn- cached-conduct-fn [combine default state dispatch-val isa?]
+  (let [st @state]
+    (or (get-in st [:cache dispatch-val])
+        (let [f (conduct-fn combine default st dispatch-val isa?)]
+          (swap! state assoc-in [:cache dispatch-val] f)
+          f))))
+
+(defn- no-conduct-error [dispatch-val]
+  (IllegalArgumentException.
+   (str "No conduct found for dispatch value: " dispatch-val)))
 
 (defn make-intent
   "Create an anonymous intention. See: defintent."
@@ -13,35 +46,15 @@
       :or   {default :default}}]
   {:pre  [(ifn? dispatch) (ifn? combine)]
    :post [(intent? %)]}
-  (let [conducts (atom {:fmap {} :cache {} :prefers #{}})
-        isa? (if hierarchy #(isa? hierarchy %1 %2) isa?)
-        func (fn [& args]
-               (let [con @conducts
-                     dv (apply dispatch args)
-                     fs (or ((:cache con) dv)
-                            (let [pref (:prefers con)
-                                  cmp (fn [a b]
-                                        (cond
-                                         (or (isa? a b) (pref [a b])) 1
-                                         (or (isa? b a) (pref [b a])) -1
-                                         :else (compare (str a) (str b))))
-                                  fs (->> (:fmap con)
-                                          (filter #(isa? dv (key %)))
-                                          (sort-by key cmp)
-                                          (mapv val))]
-                              (swap! conducts assoc-in [:cache dv] fs)
-                              fs))]
-                 (if (seq fs)
-                   (loop [ret (apply (first fs) args), fs (rest fs)]
-                     (if (seq fs)
-                       (recur (combine ret (apply (first fs) args)) (rest fs))
-                       ret))
-                   (if-let [f ((:fmap con) default)]
-                     (apply f args)
-                     (throw (IllegalArgumentException.
-                             (str "No conduct found for dispatch value: " dv)))))))]
+  (let [state (atom {:conducts {} :cache {} :prefers #{}})
+        isa?  (if hierarchy #(isa? hierarchy %1 %2) isa?)
+        func  (fn [& args]
+                (let [dv (apply dispatch args)]
+                  (if-let [f (cached-conduct-fn combine default state dv isa?)]
+                    (apply f args)
+                    (throw (no-conduct-error dv)))))]
     (with-meta func
-      (assoc (meta func) ::conducts conducts))))
+      (assoc (meta func) ::state state))))
 
 (defmacro defintent
   "Create a new intention with the associated options. The docstring and
@@ -61,34 +74,38 @@
 (defn conducts
   "Returns a map of dispatch values to conduct functions for an intention."
   [intent]
-  (-> intent meta ::conducts deref :fmap))
+  (-> intent meta ::state deref :conducts))
+
+(defmacro ^:private swap!-> [a & forms]
+  `(swap! ~a (fn [x#] (-> x# ~@forms))))
 
 (defn add-conduct
   "Adds a conduct function to an intention for the supplied dispatch value.
   See: defconduct."
   [intent dispatch-val conduct-fn]
-  (swap! (::conducts (meta intent))
-         #(-> % (assoc-in [:fmap dispatch-val] conduct-fn)
-                (assoc :cache {})))
+  (swap!-> (::state (meta intent))
+           (assoc-in [:conducts dispatch-val] conduct-fn)
+           (assoc :cache {}))
   intent)
 
 (defn remove-conduct
   "Removes a conduct function from an intention for the supplied dispatch
   value."
   [intent dispatch-val]
-  (swap! (::conducts (meta intent))
-         #(-> % (update-in [:fmap] dissoc dispatch-val)
-                (assoc :cache {})))
+  (swap!-> (::state (meta intent))
+           (update-in [:conducts] dissoc dispatch-val)
+           (assoc :cache {}))
   intent)
 
 (defn prefer-conduct
   "Causes an intention to evaluate conducts for dispatch-val-x after
   dispatch-val-y, when neither dispatch value is more specific."
   [intent dispatch-val-x dispatch-val-y]
-  (swap! (::conducts (meta intent))
-         #(-> % (update-in [:prefers] disj [dispatch-val-y dispatch-val-x])
-                (update-in [:prefers] conj [dispatch-val-x dispatch-val-y])
-                (assoc :cache {}))))
+  (swap!-> (::state (meta intent))
+           (update-in [:prefers] disj [dispatch-val-y dispatch-val-x])
+           (update-in [:prefers] conj [dispatch-val-x dispatch-val-y])
+           (assoc :cache {}))
+  intent)
 
 (defmacro defconduct
   "Creates and adds a new conduct function, associated with dispatch-val,
