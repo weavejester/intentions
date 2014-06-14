0e4763cfaa285120e892dfffb12db1ed9cb68deb
James Reeves <jreeves@weavejester.com>
2014-06-14 15:58:49 +0100

    Released 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject intentions "0.1.0"
+(defproject intentions "0.1.1"
   :description "Multimethods that combine rather than override inherited behavior"
   :url "https://github.com/weavejester/intentions"
   :license {:name "Eclipse Public License"
