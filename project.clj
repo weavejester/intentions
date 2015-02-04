d161dca4339aaff5d42537fb44e8c146287ee64f
James Reeves <jreeves@weavejester.com>
2015-02-04 01:02:10 +0000

    Update dependencies and fix cljx deploy bug

--- project.clj
+++ project.clj
@@ -3,12 +3,10 @@
   :url "https://github.com/weavejester/intentions"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.macro "0.1.2"]]
-  :plugins [[codox "0.8.9"]
-            [lein-cljsbuild "1.0.3"]
-            [com.keminglabs/cljx "0.4.0"]
-            [com.cemerick/clojurescript.test "0.3.1"]]
+  :plugins [[codox "0.8.10"]
+            [lein-cljsbuild "1.0.4"]]
   :codox {:sources ["target/generated/src"]}
   :cljx
   {:builds
@@ -18,7 +16,6 @@
     {:source-paths ["test"], :output-path "target/generated/test", :rules :cljs}]}
   :source-paths ["src" "target/generated/src"]
   :test-paths   ["test" "target/generated/test"]
-  :hooks [cljx.hooks]
   :cljsbuild
   {:builds
    [{:source-paths ["target/generated/src" "target/generated/test"]
@@ -26,8 +23,11 @@
    :test-commands {"unit-tests" ["phantomjs" :runner "target/main.js"]}}
   :aliases
   {"test-cljs" ["do" ["cljx" "once"] ["cljsbuild" "test"]]
-   "test-all"  ["do" ["with-profile" "default:+1.6" "test"] ["cljsbuild" "test"]]}
+   "test-all"  ["do" ["test"] ["cljsbuild" "test"]]}
   :profiles
-  {:provided {:dependencies [[org.clojure/clojurescript "0.0-2234"]]}
-   :dev {:dependencies [[criterium "0.4.2"]], :jvm-opts ^:replace {}}
-   :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}})
+  {:provided {:dependencies [[org.clojure/clojurescript "0.0-2760"]]}
+   :dev {:dependencies [[criterium "0.4.3"]]
+         :jvm-opts ^:replace {}
+         :plugins [[com.keminglabs/cljx "0.5.0"]
+                   [com.cemerick/clojurescript.test "0.3.3"]]
+         :prep-tasks [["cljx" "once"]]}})
