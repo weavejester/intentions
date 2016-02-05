ff9a4c3f82bd89dffbb5550d87687cabb01b7d30
James Reeves <jreeves@weavejester.com>
2016-02-05 18:29:37 +0000

    Update dependencies

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/tools.macro "0.1.2"]]
+                 [org.clojure/tools.macro "0.1.5"]]
   :plugins [[codox "0.8.13"]
-            [lein-cljsbuild "1.0.6"]]
+            [lein-cljsbuild "1.1.2"]]
   :cljsbuild
   {:builds
    [{:source-paths ["src" "test"]
@@ -18,7 +18,7 @@
    "test-cljs" ["cljsbuild" "test"]
    "test-all"  ["do" ["test"] ["test-cljs"]]}
   :profiles
-  {:provided {:dependencies [[org.clojure/clojurescript "0.0-3308"]]}
+  {:provided {:dependencies [[org.clojure/clojurescript "1.7.228"]]}
    :dev {:dependencies [[criterium "0.4.3"]]
          :jvm-opts ^:replace {}
          :plugins [[com.cemerick/clojurescript.test "0.3.3"]]}})
